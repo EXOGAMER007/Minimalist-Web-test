3ec99bb6ca2bdcaaeefe9d79fd4003a4ef7fb5eb
EXOGAMER007 <exogamer@github.com>
2026-08-02 20:11:36 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [tailwindcss()],
+  server: {
+    host: true,
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    hmr: {
+      overlay: true,
+    },
+  },
+});
